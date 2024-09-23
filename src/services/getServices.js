775fa0df63d83913b141823521445d62d9581f45
faysalahmmed1 <faysalahmmedcse@gmail.com>
2024-09-23 20:29:04 +0600

    modifition

--- src/services/getServices.js
+++ src/services/getServices.js
@@ -1,23 +1,45 @@
-export const getServices = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/services/api/get-all`);
+// export const getServices = async () => {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/services/api/get-all`);
     
+//     if (!res.ok) {
+//         throw new Error('Failed to fetch services');
+//     }
+
+//     const services = await res.json();
+//     return services;
+// }
+
+
+
+// export const getServicesDetails = async (id) => {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/services/api/${id}`);
+    
+//     if (!res.ok) {
+//         throw new Error(`Failed to fetch service with id: ${id}`);
+//     }
+
+//     const service = await res.json();
+//     return service;
+// };
+
+
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+
+export const getServices = async () => {
+    const res = await fetch(`${baseUrl}/services/api/get-all`);
     if (!res.ok) {
         throw new Error('Failed to fetch services');
     }
-
     const services = await res.json();
     return services;
-}
-
-
+};
 
 export const getServicesDetails = async (id) => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/services/api/${id}`);
-    
+    const res = await fetch(`${baseUrl}/services/api/${id}`);
     if (!res.ok) {
         throw new Error(`Failed to fetch service with id: ${id}`);
     }
-
     const service = await res.json();
     return service;
 };
